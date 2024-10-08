d81bdd8eed7e16cb39d65e67dbc58ce05f5a356a
monaceKing <leroyjulien055@gmail.com>
2024-10-08 15:33:08 +0000

    ajout de l'input pour le pourcentage de commission de YORI

--- src/app/hotels/tarif-hebergement/tarif-hebergement.component.ts
+++ src/app/hotels/tarif-hebergement/tarif-hebergement.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
@@ -13,11 +15,25 @@ import { MatIcon } from '@angular/material/icon';
     MatCardTitle,
     MatCardContent,
     MatIcon,
-    MatDivider
+    MatDivider,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './tarif-hebergement.component.html',
   styleUrl: './tarif-hebergement.component.css'
 })
 export class TarifHebergementComponent {
+  
 
+  somme: number = 0;
+  pourcentage: number = 0;
+  
+  monnaie: string = 'F CFA'; // Valeur par défaut
+
+  calculerPourcentage() {
+    this.pourcentage = this.somme * 0.15; // 15% de la somme
+    console.log(`Somme: ${this.somme}, Pourcentage: ${this.pourcentage} ${this.monnaie}`);
+  }
+  
+  
 }
